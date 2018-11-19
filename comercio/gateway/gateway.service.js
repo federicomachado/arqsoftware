938a682d908af14d9f8961ae0f0ef4dde423d1dc
Federico Machado <federicomachado@outlook.com>
2018-11-19 20:20:36 -0300

    Refactored comercio

---
+++ comercio/gateway/gateway.service.js
@@ -0,0 +1,13 @@
+const config = require("../config")
+const messages = require("../messages");
+const GatewayEntry = require("./gateway.model");
+
+exports.gateway_create = async function (res, name, category){
+    var query = { "category" : category};
+    update = { name : name, category: category },
+    options = { upsert: true, new: true, setDefaultsOnInsert: true };
+    GatewayEntry.findOneAndUpdate(query, update, options, function(error, result) {
+        if (error) return res.status(400).json({message: error});
+        return res.status(200).json({message: messages.GATEWAY.CREATE_SUCCESS});
+    });
+}
