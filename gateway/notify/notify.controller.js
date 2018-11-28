f311a34bf99433ed6808ff02e3c87b4c2f346bc3
Federico Machado <federicomachado@outlook.com>
2018-11-28 18:52:03 -0300

    Added log to Gateway and removed logic from Controllers

--- gateway/notify/notify.controller.js
+++ gateway/notify/notify.controller.js
@@ -1,44 +1,5 @@
-const superagent = require("superagent");
-const messages = require("../messages.json");
-const config = require("../config.json");
-const bcrypt = require("bcrypt");
-const Transaction = require("../transaction/transaction.model");
-const moment = require("moment");
+const NotifyService = require("./notify.service");
 
 exports.update = async function (req,res){    
-    console.log("Entered notify");    
-    let query;
-    let update;
-    req.body.params.transaction_date = moment(req.body.params.transaction_date,"DD-MM-YY HH:mm:ss").format("MM-DD-YY HH:mm:ss");
-    if (req.body.params.status == "Confirmed" ){
-        query = {
-            transaction_date : req.body.params.transaction_date,
-            name : req.body.params.name,
-        };
-        update =  {
-            transactionId : req.body.params.transactionId,
-            status : req.body.params.status
-        }
-    } else{
-        query = {
-            transactionId : req.body.params.transactionId
-        };
-        update = {
-            status: req.body.params.status
-        }
-    }
-
-    let transaction = await Transaction.find(query);
-    
-    transaction = transaction[transaction.length-1];
-    transaction.status = update.status;
-    console.log(transaction);
-    if (!transaction.transactionId){
-        transaction.transactionId = update.transactionId;
-    }
-    transaction.save();
-    
-    return res.status(200).json({ operation: "notify", params : req.body.params, provider : req.body.made_by, made_by : req.body.made_by });    
-    
-    
+    return NotifyService.update(req,res);
 }
--- gateway/verification/verification.service.js
+++ gateway/verification/verification.service.js
@@ -1,7 +1,12 @@
-const Config = require("../config.json")
+const config = require("../config.json")
+const messages = require("../messages.json");
+const bcrypt = require("bcrypt");
+const Transaction = require("../transaction/transaction.model");
+const moment = require("moment");
+const Log = require("../logs/"+config.log_service+".service");
 
 exports.getNetwork = async function ( number ){    
-    var hash = Config.networks_hash;    
+    var hash = config.networks_hash;    
     var contador = 0;
     var found = false;   
     while ( !found ) {
@@ -16,4 +21,31 @@ exports.getNetwork = async function ( number ){
         }   
         contador+=1; 
     }    
+}
+
+exports.purchase_verify = async function (req,res){
+    if (req.body.params.number && req.body.operation && req.body.params && req.body.made_by){
+        if (req.headers.authorization && bcrypt.compareSync(config.tepagoya_key,req.headers.authorization)){
+            network = await this.getNetwork(req.body.params.number);                                     
+            if (network) {                    
+                let old_date = req.body.params.transaction_date;                
+                let date = moment(req.body.params.transaction_date,"DD/MM/YY HH:mm:ss")                
+                date = date.format();
+                req.body.params.transaction_date = date;                    
+                let transaction = new Transaction(req.body.params);                    
+                transaction = await transaction.save();                
+                req.body.params.transaction_date = old_date;
+                res.status(200).json({ provider: network, made_by : req.body.made_by, operation: req.body.operation, params: req.body.params});
+            } else{
+                Log.log(messages.VERIFICATION.NETWORK_NOT_FOUND,req.body);
+                res.status(500).json({ message: messages.VERIFICATION.NETWORK_NOT_FOUND});
+            }
+        } else{
+            Log.log(messages.VERIFICATION.INVALID_KEY,req.body);
+            res.status(400).json({ message : messages.VERIFICATION.INVALID_KEY});
+        }
+    } else{        
+        Log.log(messages.VERIFICATION.INCOMPLETE_REQUEST,req.body);
+        res.status(400).json({ message : messages.VERIFICATION.INCOMPLETE_REQUEST});
+    }
 }
